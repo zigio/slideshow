c6237c3330f1df4456ee895f856482264188cd1e
martinzigio <martinzigio@gmail.com>
2021-10-31 16:19:43 +0100

    arguments and readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# slideshow
+## Create simple slideshow
+
+you will need to install following dependencies:
+    os
+    python-opencv
+    argparse
+## Arguments
+
+*`-i|--input_directory` set input dir with images. default='~/Pictures'
+*`-o|--output_directory` set output directory for video. default='~/Videos'
+*`-n|--name` set video name. default='video.avi'
+*`-s|--size` set video size, example: 960x720', default='960x720'
+*`-f|--fps` set how many pictures should be present in one frame', default='0.15'
+
---
+++ slideshow.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+import os
+import cv2
+import argparse
+
+# parse arguments
+parser = argparse.ArgumentParser(description='Create slideshow from images')
+parser.add_argument('-i', '--input_directory', help='set input dir with images', default='~/Pictures')
+parser.add_argument('-o', '--output_directory', help='set output directory for video', default='~/Videos')
+parser.add_argument('-n', '--name', help='set video name', default='video.avi')
+parser.add_argument('-s', '--size', help='set video size, example: 960x720', default='960x720')
+parser.add_argument('-f', '--fps', help='set how many pictures should be present in one frame', default='0.15')
+
+args = parser.parse_args()
+
+os.chdir(args.input_directory)
+dir_path = os.getcwd()
+
+width, height = args.size.split('x')
+
+jpg = '.jpg'
+png = '.png'
+output = args.name
+shape = int(width), int(height)
+fps = float(args.fps)
+
+# get images list
+images = [f for f in os.listdir(dir_path) if f.endswith(jpg) or f.endswith(png)]
+
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+
+os.chdir(args.output_directory)
+video = cv2.VideoWriter(output, fourcc, fps, shape)
+
+# resize image and add to video
+for image in images:
+    image_path = os.path.join(dir_path, image)
+    image = cv2.imread(image_path)
+    resized=cv2.resize(image,shape)
+    video.write(resized)
+
+video.release()
+
